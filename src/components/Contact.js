8a7e28693b95387bdec76c1f5873726233a75f8b
Alejandro Meza <56438011+jalex3421@users.noreply.github.com>
2022-08-02 16:41:15 +0200

    Add files via upload

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,13 +5,25 @@ import Card from './Card';
 function Contact(){
     return( 
         <div className="sections">
-          < Card
-            image='Alejandro' 
-            name='Alejandro Meza Tudela'
-            bio="I'm very passionate in AI and Software development!!
-            Also I’m very passionate about learning different languages. Currently leaning Japanese." 
-            linkedin="https://www.linkedin.com/in/alejandro-meza-tudela/"
-          />
+          <div class="row">
+            <div class="column">
+              < Card
+                image='Alejandro' 
+                name='Alejandro Meza Tudela'
+                bio="I'm very passionate in AI and Software development!!
+                Also I’m very passionate about learning different languages. Currently leaning Japanese." 
+                linkedin="https://www.linkedin.com/in/alejandro-meza-tudela/"
+              />
+            </div>
+            <div class="column">
+              < Card
+                image='inakiluque' 
+                name='Iñaki Luque Pastor'
+                bio="I am a computer science student who wants to test my skills about my passion." 
+                linkedin="https://www.linkedin.com/in/i%C3%B1aki-luque-pastor-7b015223a/"
+              />
+            </div>
+          </div>
         </div>
     );
 }
